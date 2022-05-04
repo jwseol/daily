438853d561a4c69aa4d2449d5b4acfe5190dad74
seoljw <jinwoo@JinwooucBookPro.box>
2022-05-04 21:41:57 +0200

    third commit

--- practice.py
+++ practice.py
@@ -1,6 +1,21 @@
-station = "사당"
-print(station + " 행 열차가 들어오고 있습니다.")
-station = "신도림"
-print(station + " 행 열차가 들어오고 있습니다.")
-station = "인천공함"
-print(station + " 행 열차가 들어오고 있습니다.")
+from random import *
+
+print(random())  # 0.0 ~ 1.0 미만의 임의의 값 생성
+print(random() * 10)  # 0.0 ~ 10.0 미만의 임의의 값 생성
+print(int(random() * 10)) # 0 ~ 10 미만의 임의의 값 생성
+print(int(random() * 10)) # 0 ~ 10 미만의 임의의 값 생성
+print(int(random() * 10)) # 0 ~ 10 미만의 임의의 값 생성
+
+print(int(random() * 49 + 1)) # 1 ~ 49 이하의 임의의 값 생성
+print(int(random() * 49 + 1)) # 1 ~ 49 이하의 임의의 값 생성
+print(int(random() * 49 + 1)) # 1 ~ 49 이하의 임의의 값 생성
+print(int(random() * 49 + 1)) # 1 ~ 49 이하의 임의의 값 생성
+print(int(random() * 49 + 1)) # 1 ~ 49 이하의 임의의 값 생성
+print(int(random() * 49 + 1)) # 1 ~ 49 이하의 임의의 값 생성
+
+print(randrange(1, 50)) # 1 ~ 50 미만의 임의의 값 생성
+
+print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+
+date = randint(4,28)
+print("오프라인 스터디 모임 날짜는 매월" + str(date) + "일로 선정되었습니다.")
